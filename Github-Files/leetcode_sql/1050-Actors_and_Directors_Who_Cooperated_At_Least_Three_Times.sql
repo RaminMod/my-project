20aa8243ac9c0f9bf96abe7f24c87f229c7e13c2
Ramin Modaresi <raminmodaressi@gmail.com>
2024-10-03 09:54:25 +0200

    ,

---
+++ Github-Files/leetcode_sql/1050-Actors_and_Directors_Who_Cooperated_At_Least_Three_Times.sql
@@ -0,0 +1,5 @@
+"""
+Write a solution to find all the pairs (actor_id, director_id) where the actor has cooperated with the director at least three times.
+
+Return the result table in any order.
+"""
