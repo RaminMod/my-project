8b4723c4a47c89a749b11cfbf7f41c131cd43f36
Ramin Modaresi <raminmodaressi@gmail.com>
2024-10-20 14:26:56 +0200

    .

--- Github-Files/leetcode_sql/176.econd_Highest_Salary.sql
+++ Github-Files/leetcode_sql/176.econd_Highest_Salary.sql
@@ -5,10 +5,7 @@ Companies:
 Amazon, Infosys
 
 Write a solution to find the second highest distinct salary from the Employee table. If there is no second highest salary, return null (return None in Pandas).
-
 The result format is in the following example.
-
- 
 
 Example 1:
 
